11a635debfddca5221d411e021afd8b9ce3235ee
iamgodot <xugodot@gmail.com>
2023-04-30 08:25:51 +0800

    Fix: remove cli.py for non-cli app

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,3 +1,9 @@
-MODULE_NAME = '{{ cookiecutter.project_slug }}'
+from os import remove
+from os.path import join
 
-print(f'Project {MODULE_NAME} created successfully.')
+MODULE_NAME = "{{ cookiecutter.project_slug }}"
+
+if "no" in "{{ cookiecutter.cli_framework|lower }}":
+    remove(join("src", MODULE_NAME, "cli.py"))
+
+print(f"Project {MODULE_NAME} created successfully.")
